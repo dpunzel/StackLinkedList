1ea1b31215d00de3716964ad7d2c6403fe8b98fe
DAVID <dpunzel15@georgefox.edu>
2019-10-14 22:53:43 -0700

    push Stack update

--- src/stacklinkedlist/Node.java
+++ src/stacklinkedlist/Node.java
@@ -8,4 +8,25 @@ public class Node<T extends Comparable<T>> {
     public Node(T data) {
         this.data = data;
     }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    @Override
+    public String toString() {
+        return this.data.toString();
+    }
 }
--- src/stacklinkedlist/Stack.java
+++ src/stacklinkedlist/Stack.java
@@ -1,4 +1,36 @@
 package stacklinkedlist;
 
-public class Stack {
+public class Stack<T extends Comparable<T>> {
+    private Node<T> root;
+    private int count;
+
+    // Push is O(1)
+    public void push(T newData) {
+        this.count++;
+
+        if (this.root == null) {
+            this.root = new Node<>(newData);
+        } else {
+            Node<T> oldRoot = this.root;
+        }
+    }
+
+    // Pop is O(1)
+    public T pop() {
+        T itemToPop = this.root.getData();
+        this.root = this.root.getNextNode();
+        this.count--;
+
+        return itemToPop;
+    }
+
+    // O(1)
+    public int size() {
+        return this.count;
+    }
+
+    // O(1)
+    public boolean isEmpty() {
+        return this.root == null;
+    }
 }
